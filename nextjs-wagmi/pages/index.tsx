f03fa33e6dc9e610f656692841719a6d38d54ad9
cacacai <jiashengcai97@gmail.com>
2022-05-22 01:59:02 +0800

    添加新的开源展示前端

---
+++ nextjs-wagmi/pages/index.tsx
@@ -0,0 +1,36 @@
+import { useState } from "react";
+import type { NextPage } from "next";
+import { useAccount, useBalance } from "wagmi";
+import { Button, Layout, Loader, WalletOptionsModal } from "../components";
+
+const Home: NextPage = () => {
+  const [showWalletOptions, setShowWalletOptions] = useState(false);
+  const [{ data: accountData, loading: accountLoading }] = useAccount();
+  const [{ data: balanceData, loading: balanceLoading }] = useBalance({
+    addressOrName: accountData?.address,
+    watch: true,
+  });
+
+  const loading = (accountLoading || balanceLoading) && !balanceData;
+
+  
+
+  return (
+    <>
+      <WalletOptionsModal
+        open={showWalletOptions}
+        setOpen={setShowWalletOptions}
+      />
+
+      <Layout
+        showWalletOptions={showWalletOptions}
+        setShowWalletOptions={setShowWalletOptions}
+      >
+        <div className="grid h-screen place-items-center">
+        </div>
+      </Layout>
+    </>
+  );
+};
+
+export default Home;
