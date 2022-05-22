52cc60ddae8bc91f05db4b74a761c5f5ee55782f
cacacai <jiashengcai97@gmail.com>
2022-05-22 09:14:19 +0800

    ant 单页

--- client/src/App.js
+++ client/src/App.js
@@ -6,35 +6,44 @@ import Create from './pages/Create';
 import Person from './pages/Person';
 import NotFound from './pages/NotFound';
 import { Link } from "react-router-dom"
-import { Layout } from 'antd';
+import { Layout, Menu } from 'antd';
+
+
 
 const { Header, Content, Footer } = Layout;
 
 function App() {
-
-  return <BrowserRouter>
-      
-    <Layout className="layout">
-    <Header>
-      <div className="logo" />
-      <Link to="/"> 首页 </Link>
-      <Link to="/detail"> 新闻页 </Link>
-      <Link to="/create"> 介绍页 </Link>
-    </Header>
-    <Content className="site-layout-content" style={{ padding: '0 50px' }}>
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/detail" element={<Detail />} />
-      <Route path="/create" element={<Create />} />
-      <Route path="/person" element={<Person />} />
-      <Route path="*" element={<NotFound />} />
-    </Routes>
-    </Content>
-    <Footer style={{ textAlign: 'center' }}>cacacai</Footer>
-  </Layout>
-     
-    
-  </BrowserRouter>
+  return (
+    <BrowserRouter>
+      <Layout>
+        <Header style={{ position: 'fixed', zIndex: 1, width: '100%' }}>
+          <div className="logo" />
+          <Menu
+            theme="dark"
+            mode="horizontal"
+            defaultSelectedKeys={['2']}
+          >
+            <Menu.Item key="1"><Link to="/">首页</Link></Menu.Item>
+            <Menu.Item key="2"><Link to="/detail">详细</Link></Menu.Item>
+            <Menu.Item key="3"><Link to="/create">创建</Link></Menu.Item>
+            <Menu.Item key="4"><Link to="/person">个人</Link></Menu.Item>
+          </Menu>
+        </Header>
+        <Content className="site-layout" style={{ padding: '0 50px', marginTop: 64 }}>
+          <div className="site-layout-background" style={{ padding: 24, minHeight: 380 }}>
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/detail" element={<Detail />} />
+              <Route path="/create" element={<Create />} />
+              <Route path="/person" element={<Person />} />
+              <Route path="*" element={<NotFound />} />
+            </Routes>
+          </div>
+        </Content>
+        <Footer style={{ textAlign: 'center' }}>cacacai</Footer>
+      </Layout>
+    </BrowserRouter>
+  )
 }
 
 
